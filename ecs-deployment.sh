a6c96eacda21a00f451adf0ad3ad4651791cc184
root <root@ip-172-31-9-190.ap-northeast-1.compute.internal>
2024-01-31 14:39:15 +0000

    asif

---
+++ ecs-deployment.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+REGION=ap-northeast-1
+ACCOUNT_ID=967866550848
+ACCESS_KEY=AKIAVRUVTVBZ5ZX2AMFX
+SECRET_KEY=igmM8hV2S5kYJYlGXe6lIv5f/DkMdroiQJf3PpdH
+COMMIT_HASH=default
+ECS_SERVICE_ARN=arn:aws:ecs:ap-northeast-1:967866550848:cluster/example
+
+# Define an array of task definitions
+TASK_DEFINITIONS=("examplegw:1" "example2:1" "example1:1")
+
+# Iterate through task definitions and update ECS service
+for TASK_DEF in "${TASK_DEFINITIONS[@]}"; do
+  aws ecs update-service \
+    --region $REGION \
+    --cluster example \
+    --service $ECS_SERVICE_ARN \
+    --task-definition $TASK_DEF
+
+  # Tag ECS tasks with the commit hash
+  aws ecs tag-resource \
+    --region $REGION \
+    --resource-arn $ECS_SERVICE_ARN \
+    --tags key=CommitHash,value=$COMMIT_HASH
+done
+
